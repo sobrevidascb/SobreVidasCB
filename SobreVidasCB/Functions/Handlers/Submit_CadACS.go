4bec4eaf736509dd4b1153ee4c426f880d956cbe
isadora-yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-07-13 15:18:40 -0300

    Delete SobreVidasCB directory

--- SobreVidasCB/Functions/Handlers/Submit_CadACS.go
+++
@@ -1,8 +0,0 @@
-package Handlers
-
-import "net/http"
-
-func submitcadacs(w http.ResponseWriter, r *http.Request){
-	ProcessCadACS(r)
-	http.Redirect(w,r,"/",http.StatusPermanentRedirect)
-}
--- SobreVidasCB/Web/Templates/JS/cadastro.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('openPopup').addEventListener('click', function() {
-    var popupElement = document.getElementById('popup');
-    popupElement.style.display = 'grid';
-});
-
-document.getElementById('voltar').addEventListener('click', function() {
-    window.location.href = "/home"
-});
